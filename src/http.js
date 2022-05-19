486f56999783491fd8e5c6bca0fea06defad12e0
Muchori Joseph <muchorijoseph97@gmail.com>
2022-05-20 01:11:05 +0300

    MicroPosts Javascript App

---
+++ src/http.js
@@ -0,0 +1,117 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP Requests, using arrow functions
+ *
+ * @version 3.0.0
+ * @author Muchori Joseph
+ * @license none
+ */
+
+class EasyHTTP {
+  /**
+   * get request
+   */
+  /* asyncget(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  } */
+  async get(url) {
+    const response = await fetch(url);
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  /**
+   * Make a post http request
+   */
+  /* post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  } */
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  /**
+   * Make a put http request
+   */
+  /* put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  } */
+
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+  /**
+   * delete request
+   */
+  /* delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then(() => resolve("Resource Deleted"))
+        .catch((err) => reject(err));
+    });
+  } */
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const resData = await "Resource Deleted";
+    return resData;
+  }
+}
+
+export const http = new EasyHTTP();
